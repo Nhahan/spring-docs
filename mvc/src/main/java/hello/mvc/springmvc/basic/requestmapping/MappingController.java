32059b8bc0eec40f550f463d2d4c9f7271bcff2a
unknown <kisy324@naver.com>
2022-10-24 11:43:50 +0900

    GetMapping

--- mvc/src/main/java/hello/mvc/springmvc/basic/requestmapping/MappingController.java
+++ mvc/src/main/java/hello/mvc/springmvc/basic/requestmapping/MappingController.java
@@ -1,14 +1,14 @@
 package hello.mvc.springmvc.basic.requestmapping;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class MappingController {
 
-    @RequestMapping({"/hello-basic", "/hello-go"})
+    @GetMapping({"/hello-basic", "/hello-go"})
     public String helloBasic() {
         log.info("helloBasic");
         return "ok";
