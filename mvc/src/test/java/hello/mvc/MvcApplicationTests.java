8c5fea967791e2fa6354c1436edef342c17637b9
unknown <suny@nm-metaworld.com>
2022-10-24 10:12:32 +0900

    reinit

--- mvc/src/test/java/hello/mvc/MvcApplicationTests.java
+++ mvc/src/test/java/hello/mvc/MvcApplicationTests.java
@@ -1,13 +1,13 @@
-package mvc.spring;
+package hello.mvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class MvcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
