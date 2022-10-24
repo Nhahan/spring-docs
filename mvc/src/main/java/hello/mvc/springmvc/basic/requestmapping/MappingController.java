a22de99e30f364d1d76db1f0900eb83be7cd4df8
unknown <kisy324@naver.com>
2022-10-24 11:56:23 +0900

    Additional parameter mapping

--- mvc/src/main/java/hello/mvc/springmvc/basic/requestmapping/MappingController.java
+++ mvc/src/main/java/hello/mvc/springmvc/basic/requestmapping/MappingController.java
@@ -13,4 +13,18 @@ public class MappingController {
         log.info("helloBasic");
         return "ok";
     }
+
+    /**
+     * 파라미터로 추가 매핑
+     * params="mode",
+     * params="!mode",
+     * params="mode=debug",
+     * params="mode!=debug"
+     * params={"mode-debug", "data=good"}
+     */
+    @GetMapping(value="/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
 }
