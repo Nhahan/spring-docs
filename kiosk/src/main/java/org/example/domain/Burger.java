820ab1911ea33daa6dd5cb169762ead43cce85d7
Nhahan <kisy324@naver.com>
2024-01-03 01:32:03 +0900

    주문 클래스도 만들어서 상품 객체를 담을 수 있도록 해주세요.

--- kiosk/src/main/java/org/example/domain/Burger.java
+++ kiosk/src/main/java/org/example/domain/Burger.java
@@ -4,13 +4,25 @@ package org.example.domain;
 // 상품 클래스의 이름, 설명 필드는 메뉴 클래스를 상속받아 사용하는 구조로 개발해주세요.
 public class Burger extends Menu {
 
-    private String name;
-    private String description;
-    private int price;
+    private final String name;
+    private final String description;
+    private final int price;
 
     public Burger(String name, String description, int price) {
         this.name = name;
         this.description = description;
         this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
