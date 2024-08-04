4584c3aedb2a1c9baa9c07cceb3c3da2570268da
Nhahan <kisy324@naver.com>
2024-08-04 18:08:46 +0900

    init object

---
+++ object/src/main/java/org/example/object/ObjectApplication.java
@@ -0,0 +1,12 @@
+package org.example.object;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
