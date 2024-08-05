9f06b5edc5d04d4db3756a49e399197d67a582f1
Nhahan <kisy324@naver.com>
2024-08-05 23:28:08 +0900

    Extract Method: getExRate()

--- object/src/main/java/org/example/object/PaymentService.java
+++ object/src/main/java/org/example/object/PaymentService.java
@@ -5,6 +5,7 @@ import lombok.SneakyThrows;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
@@ -16,7 +17,14 @@ public class PaymentService {
 
     @SneakyThrows
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
-        // 환율 가져오기
+        BigDecimal exRate = getExRate(currency);
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    private BigDecimal getExRate(String currency) throws IOException {
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection connection = (HttpsURLConnection) url.openConnection();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -25,15 +33,7 @@ public class PaymentService {
 
         ObjectMapper objectMapper = new ObjectMapper();
         ExRate exrate = objectMapper.readValue(response, ExRate.class);
-
         BigDecimal exRate = exrate.getRates().get("KRW");
-
-        // 금액 계산
-        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-
-        // 유효 시간 계산
-        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
-
-        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+        return exRate;
     }
 }
