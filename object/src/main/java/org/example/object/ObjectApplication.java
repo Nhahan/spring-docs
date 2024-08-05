d71da6d7bb29b960091156e66edf4423b501e45d
Nhahan <kisy324@naver.com>
2024-08-05 23:08:12 +0900

    PaymentService 초기

--- object/src/main/java/org/example/object/ObjectApplication.java
+++ object/src/main/java/org/example/object/ObjectApplication.java
@@ -2,11 +2,14 @@ package org.example.object;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
+
 @SpringBootApplication
 public class ObjectApplication {
 
     public static void main(String[] args) {
-
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(55.5));
+        System.out.println(payment);
     }
-
 }
