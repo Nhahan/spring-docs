fa7b8a6a5ecb2ede7bbfe015ba7c2b5bda076975
Nhahan <kisy324@naver.com>
2025-01-27 12:04:45 +0900

    fix: jdbc example `MemoRequestDto` id 제거`

---
+++ jdbc/src/main/java/com/example/jdbc/controller/MemoController.java
@@ -0,0 +1,42 @@
+package com.example.jdbc.controller;
+
+import com.example.jdbc.dto.MemoRequestDto;
+import com.example.jdbc.dto.MemoResponseDto;
+import com.example.jdbc.service.MemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemoController {
+
+    private final MemoService memoService;
+
+    @PostMapping("/memos")
+    public ResponseEntity<MemoResponseDto> saveMemo(@RequestBody MemoRequestDto dto) {
+        return ResponseEntity.ok(memoService.saveMemo(dto));
+    }
+
+    @GetMapping("/memos")
+    public ResponseEntity<List<MemoResponseDto>> findById() {
+        return ResponseEntity.ok(memoService.findAll());
+    }
+
+    @GetMapping("/memos/{id}")
+    public ResponseEntity<MemoResponseDto> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(memoService.findById(id));
+    }
+
+    @PutMapping("/memos/{id}")
+    public ResponseEntity<MemoResponseDto> updateContent(@PathVariable Long id, @RequestBody MemoRequestDto dto) {
+        return ResponseEntity.ok(memoService.updateContent(id, dto));
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public void delete(@PathVariable Long id) {
+        memoService.delete(id);
+    }
+}
---
+++ jdbc/src/main/java/com/example/jdbc/repository/MemoRepository.java
@@ -0,0 +1,15 @@
+package com.example.jdbc.repository;
+
+import com.example.jdbc.entity.Memo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemoRepository {
+
+    Memo save(Memo memo);
+    Optional<Memo> findById(Long id);
+    List<Memo> findAll();
+    Memo updateContent(Long id, String content);
+    void delete(Memo memo);
+}
