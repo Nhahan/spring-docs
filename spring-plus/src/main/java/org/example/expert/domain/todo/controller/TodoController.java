12893871acbe62eb8c034607c6393aff51803a84
Nhahan <kisy324@naver.com>
2025-03-21 03:09:50 +0900

    Lv1 - 10

--- spring-plus/src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ spring-plus/src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.todo.dto.TodoSearchDto;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
@@ -26,6 +27,18 @@ public class TodoController {
             @Valid @RequestBody TodoSaveRequest todoSaveRequest
     ) {
         return ResponseEntity.ok(todoService.saveTodo(authUser, todoSaveRequest));
+    }
+
+    @GetMapping("/todos/search")
+    public ResponseEntity<Page<TodoSearchDto>> search(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) String managerNickname,
+            @RequestParam(required = false) LocalDateTime startAt,
+            @RequestParam(required = false) LocalDateTime endAt
+    ) {
+        return ResponseEntity.ok(todoService.search(page, size, title, managerNickname, startAt, endAt));
     }
 
     @GetMapping("/todos")
